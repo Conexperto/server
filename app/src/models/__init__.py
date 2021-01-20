7e68b67a30cbf123f0c294ee06c6f98bbf608b49
FrFernandez <frfernandez1996@gmail.com>
2021-01-19 23:07:00 -0400

    feat: add import to __init__

--- app/src/models/__init__.py
+++ app/src/models/__init__.py
@@ -1,2 +1,11 @@
-from .auth import AuthModel
+from .auth import Auth
+from .auth_admin import AuthAdmin
+from .admin import Admin
+from .user import User
+from .expert import Expert
+from .method import Method
+from .plan import Plan
+from .speciality import Speciality
+from .session import Session
+
 from .test import TestModel
