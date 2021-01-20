5b9ba67d8dfc54c86e90eda1cd5060061b10a86f
FrFernandez <frfernandez1996@gmail.com>
2021-01-19 23:06:15 -0400

    feat: add new routes

--- app/src/blueprints/auth.py
+++ app/src/blueprints/auth.py
@@ -6,7 +6,7 @@ from src.services import AuthService
 
 router = Blueprint(name='Auth', import_name=__name__)
 
-
+# Decorador
 def get_token():
     headers = request.headers
     prefix = 'Bearer '
@@ -21,7 +21,7 @@ def get_token():
 
     return token[len(prefix):]
 
-
+# Decorador
 def login_required(func):
     @wraps(func)
     def wrap(*args, **kwargs):
@@ -36,15 +36,50 @@ def login_required(func):
 
     return wrap
 
-
+# GET: /api/v1/auth
 @router.route('/', methods=['GET'])
 @login_required
 def index():
     return jsonify(g.user);
 
-
+# POST: /api/v1/auth
 @router.route('/', methods=['POST'])
 def register():
-    return jsonify(request.json);
+    body = request.json
 
+    service = AuthService()
+    service.create_user(body)
+
+    return jsonify(service.response());
+
+# PUT: /api/v1/auth
+@router.route('/', methods=['PUT'])
+def update():
+    body = request.json
+    user = g.user
+
+    service = AuthService()
+    service.update_user(user, body)
+
+    return jsonify(service.response())
+
+# PATCH: /api/v1/auth
+@router.route('/', methods=['PATCH'])
+def update_field():
+    body = request.json
+    user = g.user
+
+    service = AuthService()
+    service.update_field_user(user, body)
+
+    return jsonify(service.response())
+
+# DELETE: /api/v1/auth
+@router.route('/', methods=['DELETE']):
+    user = g.user
+
+    service = AuthService()
+    service.delete_user(user)
+
+    return jsonify(service.response())
 
