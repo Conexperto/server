229aeede30ffaf19f09b48002dbad45a3ceb5932
FrFernandez <frfernandez1996@gmail.com>
2021-01-19 23:12:32 -0400

    feat: add some methods

--- app/src/services/auth.py
+++ app/src/services/auth.py
@@ -1,13 +1,28 @@
-from src.models import AuthModel
+from src.models import Auth
 
 
 
 class AuthService():
 
     def __init__(self):
-        self.__auth = AuthModel()
+        self.__auth = Auth()
 
 
     def authentication(self, id_token):
         return self.__auth.authentication(id_token)
 
+    
+    def create_user(self, user, body):
+        pass
+
+    def update_user(self, user, body):
+        pass
+
+    def update_field_user(self, user, body):
+        pass
+
+    def delete_user(self, user, body):
+        pass
+
+    def response(self):
+        return self.response
