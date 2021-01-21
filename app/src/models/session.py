5373d7c7233bcf4900bcbcf6502837236c0ae9f8
FrFernandez <frfernandez1996@gmail.com>
2021-01-21 12:05:57 -0400

    feat: add associate table

--- app/src/models/session.py
+++ app/src/models/session.py
@@ -2,20 +2,18 @@ from src.mixins import BaseMixin
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy import String, Integer, ARRAY, DateTime
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
+from src.db import db
 
 
-Base = declarative_base()
 
-
-class AssociationUser(BaseMixin, Base):
+class AssociationUser(BaseMixin, db.Model):
     __tablename__ = 'association_session_to_user'
     left_id          = Column(Integer, ForeignKey('session.id'), primary_key=True)
     right_id         = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
 
-class Session(BaseMixin, Base):
+class Session(BaseMixin, db.Model):
     __tablename__ = 'session'
 
     id              = Column(Integer, primary_key=True)
