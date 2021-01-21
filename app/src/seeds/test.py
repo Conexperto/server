b6d83a0ca4609d45ea44795f86c4ef7fbca1bd59
FrFernandez <frfernandez1996@gmail.com>
2021-01-21 12:15:11 -0400

    refactor: remove extends command

--- app/src/seeds/test.py
+++ app/src/seeds/test.py
@@ -1,13 +1,13 @@
-from flask_script import Command
 from faker import Faker
 from src.models import TestModel
 
 faker = Faker() 
 
-class TestSeed(Command, TestModel):
+class TestSeed():
+    __seed__ = 'test'
 
     def __init__(self):
-        self.name = faker.name();  
+        self.__model = TestModel(name=faker.name())  
     
     def run(self):
-        self.save();
+        self.__model.save();
