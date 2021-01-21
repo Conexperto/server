740c3ef5af4ba8fc0c908b90c954b41d401dfcb6
FrFernandez <frfernandez1996@gmail.com>
2021-01-21 12:24:57 -0400

    refactor: remove testSeed by SeedCommand

--- app/manage.py
+++ app/manage.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from config import Config
 from src.api import create_api, db
-from src.seeds import TestSeed
+from seed import SeedCommand
 
 #assert os.system('service postgresql status') == 0, 'Not postgresql daemon running.'
 
@@ -19,7 +19,7 @@ manager = Manager(api)
 manager.add_command('db', MigrateCommand)
 
 # Comamnd of seed 
-manager.add_command('seed', TestSeed())
+manager.add_command('seed', SeedCommand())
 
 if __name__ == "__main__":
     manager.run()
