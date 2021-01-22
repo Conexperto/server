b8caeab13bf1aebb44eb8ad4846c6f7f3e23c94e
FrFernandez <frfernandez1996@gmail.com>
2021-01-22 12:58:11 -0400

    refactor: resolve imports

--- app/src/seeds/__init__.py
+++ app/src/seeds/__init__.py
@@ -2,3 +2,7 @@ from .test import TestSeed
 from .admin import AdminSeed
 from .user import UserSeed
 from .expert import ExpertSeed
+from .method import MethodSeed
+from .plan import PlanSeed
+from .speciality import SpecialitySeed
+
