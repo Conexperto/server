5aac7276894405d7fd153d88e06c758a1032783a
FrFernandez <frfernandez1996@gmail.com>
2021-01-22 12:57:51 -0400

    refactor: resolve imports

--- app/src/models/__init__.py
+++ app/src/models/__init__.py
@@ -2,7 +2,7 @@ from .auth import Auth
 from .auth_admin import AuthAdmin
 from .admin import Admin, Privilegies
 from .user import User
-from .expert import Expert
+from .expert import Expert, AssociationSpeciality, AssociationMethod
 from .method import Method
 from .plan import Plan
 from .speciality import Speciality
