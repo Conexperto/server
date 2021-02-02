2beb3ddf0df9f64023f9c903dd84b9ae009352f4
Martin76ec <larreaamartin@gmail.com>
2021-02-02 02:31:22 -0500

    feat: added postgres update for user.display_name

--- app/src/services/auth.py
+++ app/src/services/auth.py
@@ -1,7 +1,5 @@
 from src.models import Auth, User
 
-
-
 class AuthService():
 
     def __init__(self):
@@ -11,24 +9,43 @@ class AuthService():
     def authentication(self, id_token):
         return self.__auth.authentication(id_token)
 
-    
     def create_user(self, body):
         user_record = self.__auth.create_user(
                         email=body['email'],
                         password=body['password'],
                         display_name=body['display_name'])
-        
+
         user = User(uid=user_record.uid,
                         email=body['email'],
                         display_name=body['display_name'])
-        
+
         user.add()
         user.save()
 
         return user
 
+    def get_userById(self, body):
+        user = User.query.filter_by(id=body['id']).first()
+        return user
+
+    def get_userByUid(self, body):
+        user = User.query.filter_by(uid=body['uid']).first()
+        return user
+
+    def get_userByEmail(self, body):
+        user = User.query.filter_by(email=body['email']).first()
+        return user
+
     def update_user(self, user, body):
-        pass
+        user = user['b']
+        # insert changes 
+
+        if(user.display_name != body['display_name']):
+            user.display_name = body['display_name']
+
+        user.save()
+
+        return user
 
     def update_field_user(self, user, body):
         pass
