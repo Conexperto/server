d0b8f1005a9315f50f38147b535ba9aac130b0e1
FrFernandezdev <frfernandezdev@gmail.com>
2021-02-06 19:47:46 -0400

    refactor: update imports

--- app/src/blueprints/__init__.py
+++ app/src/blueprints/__init__.py
@@ -1,2 +1,4 @@
 from .auth import router as auth
-from .auth import router as auth_admin
+from .auth_admin import router as auth_admin
+from .user import router as user
+from .admin import router as admin
