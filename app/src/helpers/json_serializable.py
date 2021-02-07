d63fa210df7c9e9f1324d1cbe49f78a9228a9c3a
FrFernandezdev <frfernandezdev@gmail.com>
2021-02-06 20:40:48 -0400

    feat: add class deserializable

--- app/src/helpers/json_serializable.py
+++ app/src/helpers/json_serializable.py
@@ -6,31 +6,18 @@ from datetime import datetime
 
 try:
     from flask_sqlalchemy.model import Model
+    from src.mixins import Record
 except ImportError as e:
     print(str(e))
 
 class JSONEncoder(json.JSONEncoder):
     
     def default(self, o):
-        def recursive(p, o=None):
-            result = OrderedDict()
-            for k in p.keys():
-                if k in o._repr_hide:
-                    continue
-                result[k] = getattr(o, k);
-
-                if isinstance(result[k], dict):
-                    result[k] = recursive(result[k])
-                if isinstance(result[k], datetime):
-                    result[k] = result[k].timestamp()
-                
-            return result
-
         if isinstance(o, Model):
-            return dict(recursive(o.__mapper__.c, o))
-        
-        if inspect.isclass(type(o)):
-            return dict(o.__dict__)
+            return dict(o.deserialize())
+       
+        if isinstance(o, Record):
+            return dict(o.deserialize())
 
         return flask.json.JSONEncoder.default(self, o)
 
