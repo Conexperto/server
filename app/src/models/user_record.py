0dafef594ab9b11bf5b0cd0f14c65abf9508bbfd
FrFernandezdev <frfernandezdev@gmail.com>
2021-02-06 20:31:02 -0400

    feat: add record mixins

--- app/src/models/user_record.py
+++ app/src/models/user_record.py
@@ -1,32 +1,31 @@
 from flask import abort
 from firebase_admin import auth
+from src.mixins import Record
 
 
-
-class UserRecord():
+class UserRecord(Record):
 
     @staticmethod
     def verify_id_token(id_token, check_revoked, app):
         try:
             return auth.verify_id_token(id_token, check_revoked=check_revoked, app=app)
         except auth.ExpiredIdTokenError as ex:
-            raise abort(401, description='ExpiredIdToken')
-        except auth.InvalidIdTokenError as ex:
-            raise abort(401, description='InvalidIdToken')
+            raise abort(401, description='ExpiredIdToken', response='auth/id-token-expired')
         except auth.RevokedIdTokenError as ex:
-            raise abort(401, description='RevokedIdToken')
+            raise abort(401, description='RevokedIdToken', response='auth/id-token-revoked')
+        except auth.InvalidIdTokenError as ex:
+            raise abort(401, description='InvalidIdToken', response='auth/invalid-id-token')
         except auth.TokenSignError as ex:
             raise abort(401, description='TokenSign')
         except auth.UnexpectedResponseError as ex:
             raise abort(500, description='UnexpectedResponse')
-
 
     @classmethod
     def get_user(cls, uid, app):
         try:
             return cls(auth.get_user(uid, app=app), app=app)
         except auth.UserNotFoundError as ex:
-            raise abort(404, description='UserNotFound')
+            raise abort(404, description='UserNotFound', response='auth/user-not-found')
         except auth.UnexpectedResponseError as ex:
             raise abort(500, description='UnexpectedResponse')
 
@@ -38,20 +37,20 @@ class UserRecord():
                                 password=password,
                                 display_name=display_name, app=app), app=app)
         except auth.UidAlreadyExistsError as ex:
-            raise abort(400, description='UidAlreadyExists')
+            raise abort(400, description='UidAlreadyExists', response='auth/uid-already-exists')
         except auth.EmailAlreadyExistsError as ex:
-            raise abort(400, description='EmailAlreadyExists', response='auth/email-already-in-use')
+            raise abort(400, description='EmailAlreadyExists', response='auth/email-already-exists')
         except auth.PhoneNumberAlreadyExistsError as ex:
-            raise abort(400, description='PhoneNumberAlreadyExists')
+            raise abort(400, description='PhoneNumberAlreadyExists', response='auth/phone-number-already-exists')
         except auth.UnexpectedResponseError as ex:
             raise abort(500, description='UnexpectedResponse')
 
     def __init__(self, user_record, app):
         self.app = app
-
         self.uid = user_record.uid
         self.email = user_record.email
         self.email_verified = user_record.email_verified
+        self.password = user_record.password if hasattr(user_record, 'password') else None
         self.display_name = user_record.display_name
         self.phone_number = user_record.phone_number
         self.photo_url = user_record.photo_url
@@ -71,28 +70,30 @@ class UserRecord():
 
     def update_user(self):
         try:
-            auth.update_user(
-                    uid=self.uid,
-                    email=self.email,
-                    password=self.password,
-                    display_name=self.display_name,
-                    phone_number=self.phone_number,
-                    photo_url=self.photo_url,
-                    disabled=self.disabled, app=self.app)
+            args = { 'app': self.app };
+
+            for k,v in self.__dict__.items():
+                if k in ['app', 'provider_data', 'custom_claims', 'tokens_valid_after_timestamp']:
+                    continue
+                if not v:
+                    continue
+                args[k] = v
+
+            auth.update_user(**args)
         except auth.UidAlreadyExistsError as ex:
-            raise abort(400, description='UidAlreadyExists')
+            raise abort(400, description='UidAlreadyExists', response='auth/uid-already-exists')
         except auth.EmailAlreadyExistsError as ex:
-            raise abort(400, description='EmailAlreadyExists')
+            raise abort(400, description='EmailAlreadyExists', response='auth/email-already-exists')
         except auth.PhoneNumberAlreadyExistsError as ex:
-            raise abort(400, description='PhoneNumberAlreadyExists')
+            raise abort(400, description='PhoneNumberAlreadyExists', response='auth/phone-number-already-exists')
         except auth.UnexpectedResponseError as ex:
             raise abort(500, description='UnexpectedResponse')
 
     def delete_user(self):
         try:
-            auth._delete_user(uid=self.uid, app=self.app)
+            auth.delete_user(uid=self.uid, app=self.app)
         except auth.UserNotFoundError as ex:
-            raise abort(404, description='UserNotFound')
+            raise abort(404, description='UserNotFound', response='auth/user-not-found')
         except auth.UnexpectedResponseError as ex:
             raise abort(500, description='UnexpectedResponse')
 
@@ -100,12 +101,7 @@ class UserRecord():
         try:
             auth.set_custom_user_claims(self.uid, claims, app=self.app)
         except auth.UserNotFoundError as ex:
-            raise abort(404, description='UserNotFound')
+            raise abort(404, description='UserNotFound', response='auth/user-not-found')
         except auth.UnexpectedResponseError as ex:
             raise abort(500, description='UnexpectedResponse')
     
-    def serialize(self, obj):
-        for key, val in obj.items():
-            if key in self.keys():
-                self[key] = val
-        return self
