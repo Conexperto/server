b34da5056841f605efed2845a7948722d5a9714a
FrFernandezdev <frfernandezdev@gmail.com>
2021-02-06 20:27:46 -0400

    feat: serialize and deserialize

--- app/src/mixins/base.py
+++ app/src/mixins/base.py
@@ -1,5 +1,7 @@
+from collections import OrderedDict
 from src.db import db 
 from .audit import AuditMixin
+from datetime import datetime
 
 
 class BaseMixin(AuditMixin):
@@ -16,12 +18,24 @@ class BaseMixin(AuditMixin):
         db.session.commit()
     
     def serialize(self, obj):
-        for key, val in obj.items():
-            if key in self.__mapper__.c.keys():
-                self[key] = val
+        for k, v in obj.items():
+            if k in self.__table__.c.keys() and v:
+                setattr(self, k, v)
+        return self
 
-    def deserialize(self):
-        pass
+    def deserialize(self, o=None):
+        result = OrderedDict()
+        for k in self.__table__.c.keys():
+            if k in self._repr_hide:
+                continue
+            result[k] = getattr(self, k)
+
+            if isinstance(result[k], dict):
+                result[k] = self.deserialize(result[k])
+            if isinstance(result[k], datetime):
+                result[k] = result[k].timestamp()
+
+        return result
 
     def __repr__(self):
         vals = ', '.join("%s=%r" % (n, getattr(self,n)) for n in self.__table__.c.keys() if n not in self._repr_hide)
