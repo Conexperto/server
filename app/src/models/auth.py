bb1902ea1800844959ff7c2a7a4f1ca7a3c74ad6
FrFernandezdev <frfernandezdev@gmail.com>
2021-02-11 20:29:50 -0400

    refactor: add response to abort

--- app/src/models/auth.py
+++ app/src/models/auth.py
@@ -1,8 +1,8 @@
 from flask import abort
 from firebase_admin import auth
 from src.firebase import web_sdk
-from .user import User
 from .user_record import UserRecord
+from .user import User
 
 
 class Auth():
@@ -12,10 +12,10 @@ class Auth():
         user_record = UserRecord.get_user(decoded_token['uid'], app=web_sdk)
         
         if user_record.disabled:
-            raise abort(401,'AccountDisabled')
+            raise abort(401, description='AccountDisabled', response='auth/account-disabled')
     
         claims = user_record.custom_claims;
-
+    
         user = User.query.filter_by(uid=user_record.uid).first()
 
         return {
