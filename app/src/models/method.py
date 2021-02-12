0f5a8d56f3093aecac48e108910c57aa0d620fd6
FrFernandezdev <frfernandezdev@gmail.com>
2021-02-11 20:26:53 -0400

    feat: add disabled column

--- app/src/models/method.py
+++ app/src/models/method.py
@@ -11,4 +11,5 @@ class Method(BaseMixin, db.Model):
 
     id              = Column(Integer, primary_key=True)
     name            = Column(String, nullable=False)
+    disabled        = Column(Boolean, default=False)
 
--- app/src/models/session.py
+++ app/src/models/session.py
@@ -1,6 +1,6 @@
 from src.mixins import BaseMixin
 from sqlalchemy import Column, ForeignKey
-from sqlalchemy import String, Integer, ARRAY, DateTime
+from sqlalchemy import String, Integer, ARRAY, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from src.db import db
@@ -9,8 +9,10 @@ from src.db import db
 
 class AssociationUser(BaseMixin, db.Model):
     __tablename__ = 'association_session_to_user'
+
     left_id          = Column(Integer, ForeignKey('session.id'), primary_key=True)
     right_id         = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    disabled         = Column(Boolean, default=False)
 
 
 class Session(BaseMixin, db.Model):
@@ -20,5 +22,6 @@ class Session(BaseMixin, db.Model):
     date_start      = Column(DateTime, nullable=False)
     date_end        = Column(DateTime, nullable=False)
     duration        = Column(Integer, nullable=False)
+    disabled        = Column(Boolean, default=False)
     expert_id       = Column(Integer, ForeignKey('user.id'))
 
