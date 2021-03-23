25dafbc12c5a6b9d251ed8894603f32a144a80e0
FrFernandezdev <frfernandezdev@gmail.com>
2021-03-22 23:31:11 -0400

    refactor: change uid to _id

--- app/src/blueprints/speciality.py
+++ app/src/blueprints/speciality.py
@@ -5,8 +5,8 @@ from src.services import SpecialityService
 
 router = Blueprint(name='Speciality', import_name=__name__)
 
-# GET: /api/v1/speciality/<int:id>
-@router.route('/<int:id>', methods=['GET'])
+# GET: /api/v1/speciality/<int:_id>
+@router.route('/<int:_id>', methods=['GET'])
 def index_speciality_one(_id):
     service = SpecialityService()
     speciality = service.get(_id)
