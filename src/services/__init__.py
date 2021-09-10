6df4602af54ef18a481e9d64f6da3d21e744375e
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-10 17:36:08 -0400

    refactor(services): delete export

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,10 +1,8 @@
 """ __init__ """
 from src.services.admin import AdminService
 from src.services.admin import Privileges
-from src.services.association_method import AssociationExpertToMethodService
-from src.services.association_speciality import (
-    AssociationExpertToSpecialityService,
-)
+from src.services.association_method import AssociationMethod
+from src.services.association_speciality import AssociationSpeciality
 from src.services.auth import AuthService
 from src.services.auth_admin import AuthAdminService
 from src.services.method import MethodService
@@ -12,16 +10,13 @@ from src.services.plan import PlanService
 from src.services.speciality import SpecialityService
 from src.services.user import UserService
 
-# from src.services.expert import ExpertService
-
 
 __all__ = [
     "AdminService",
-    "AssociationExpertToMethodService",
-    "AssociationExpertToSpecialityService",
+    "AssociationMethod",
+    "AssociationSpeciality",
     "AuthService",
     "AuthAdminService",
-    #   "ExpertService",
     "MethodService",
     "PlanService",
     "SpecialityService",
