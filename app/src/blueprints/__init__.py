7edba7a7ca19c4a14983d680d4492226809c0de5
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-10 21:49:34 -0400

    refactor: delete folder app

--- app/src/blueprints/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import router as auth
-from .expert import router as expert
-from .method import router as method
-from .search import router as search
-from .speciality import router as speciality
-from .user import router as user
