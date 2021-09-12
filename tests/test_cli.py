243fb3257af8b639ff3905f41472ce082b13dd18
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-11 22:47:00 -0400

    ci: add logger info

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,21 +1,13 @@
 """ tests.test_cli """
-import logging
-
-
-logger = logging.getLogger(__name__)
 
 
 def test_seed_user(runner):
     result_up = runner.invoke(args=["seed", "user", "up"])
     result_down = runner.invoke(args=["seed", "user", "down"])
-    logger.info(str(result_up))
-    logger.info(str(result_down))
     assert result_up.exit_code == 0 and result_down.exit_code == 0
 
 
 def test_seed_admin(runner):
     result_up = runner.invoke(args=["seed", "admin", "up"])
     result_down = runner.invoke(args=["seed", "admin", "down"])
-    logger.info(result_up)
-    logger.info(result_down)
     assert result_up.exit_code == 0 and result_down.exit_code == 0
