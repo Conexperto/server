ce5bb45176e102ed62cfc18725695d76dd6f4215
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-13 06:05:31 -0400

    test: split asserts

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,11 +7,13 @@ logger = logging.getLogger(__name__)
 
 def test_seed_user(runner):
     result_up = runner.invoke(args=["seed", "user", "up"])
+    assert result_up.exit_code == 0
     result_down = runner.invoke(args=["seed", "user", "down"])
-    assert result_up.exit_code == 0 and result_down.exit_code == 0
+    assert result_down.exit_code == 0
 
 
 def test_seed_admin(runner):
     result_up = runner.invoke(args=["seed", "admin", "up"])
+    assert result_up.exit_code == 0
     result_down = runner.invoke(args=["seed", "admin", "down"])
-    assert result_up.exit_code == 0 and result_down.exit_code == 0
+    assert result_down.exit_code == 0
