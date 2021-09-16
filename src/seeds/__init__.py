ee0950cdac05bd4febdd9c5b3d49396d11780ec7
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-15 20:20:10 -0400

    fix: seeds

--- src/seeds/__init__.py
+++ src/seeds/__init__.py
@@ -1,15 +1,9 @@
 """ __init__ """
 from src.seeds.admin import AdminSeed
-from src.seeds.method import MethodSeed
-from src.seeds.plan import PlanSeed
-from src.seeds.speciality import SpecialitySeed
 from src.seeds.user import UserSeed
 
 
 __all__ = [
     "AdminSeed",
-    "MethodSeed",
-    "PlanSeed",
-    "SpecialitySeed",
     "UserSeed",
 ]
