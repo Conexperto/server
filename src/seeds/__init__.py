e3639f7bb8d77c591eae34b255e3a79528cdf009
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-20 21:19:05 -0400

    feat(seed): update imports

--- src/seeds/__init__.py
+++ src/seeds/__init__.py
@@ -1,9 +1,8 @@
 """ __init__ """
 from src.seeds.admin import AdminSeed
+from src.seeds.method import MethodSeed
+from src.seeds.speciality import SpecialitySeed
 from src.seeds.user import UserSeed
 
 
-__all__ = [
-    "AdminSeed",
-    "UserSeed",
-]
+__all__ = ["AdminSeed", "UserSeed", "SpecialitySeed", "MethodSeed"]
