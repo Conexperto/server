cf5eaf80f236faf698c2bdb4bcdeaa0adfe9798a
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-20 21:13:27 -0400

    test: add drop_all to firebase auth

--- tests/confauth.py
+++ tests/confauth.py
@@ -3,6 +3,9 @@ import logging
 import os
 
 import requests
+from firebase_admin import auth
+
+from src.firebase import admin_sdk
 
 
 logger = logging.getLogger(__name__)
@@ -19,10 +22,7 @@ class AuthActions(object):
     def login(self, email, password):
         uri = URI.format("signInWithPassword", self._key)
         payload = {"email": email, "password": password, "returnSecureToken": True}
-        logger.info(uri)
-        logger.info(payload)
         rv = requests.post(uri, json=payload)
-        logger.info(rv.json())
         assert rv.status_code == 200, "Faild authentication"
         self._user = rv.json()
         self._token = self._user["idToken"]
@@ -46,3 +46,11 @@ class AuthActions(object):
     @property
     def refresh_token(self):
         return self._refresh_token
+
+    def drop_all(self):
+        identifiers = []
+        page = auth.list_users(app=admin_sdk)
+        while page:
+            identifiers.extend([item.uid for item in page.users])
+            page = page.get_next_page()
+        auth.delete_users(identifiers, app=admin_sdk)
