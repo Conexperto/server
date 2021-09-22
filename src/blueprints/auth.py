e64b75a63f15e34a5f60ce9c9b648f512e5b948a
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-22 19:17:37 -0400

    fix(blueprints): add operation many endpoint

--- src/blueprints/auth.py
+++ src/blueprints/auth.py
@@ -21,6 +21,28 @@ def index_auth():
     return jsonify({"success": True, "response": g.user})
 
 
+@router.route("/specialities", methods=["GET"])
+@login_required()
+def index_auth_specialities():
+    """
+    GET: /api/v1/auth/specialities
+    """
+    user = g.user["b"]
+    specialities = user.specialities
+    return jsonify({"success": True, "response": specialities})
+
+
+@router.route("/methods", methods=["GET"])
+@login_required()
+def index_auth_methods():
+    """
+    GET: /api/v1/auth/methods
+    """
+    user = g.user["b"]
+    methods = user.methods
+    return jsonify({"success": True, "response": methods})
+
+
 @router.route("/", methods=["POST"])
 def register_auth():
     """
@@ -31,6 +53,9 @@ def register_auth():
 
         if not body:
             raise HandlerException(400, "Not found body request")
+
+        if not isinstance(body, dict):
+            raise HandlerException(400, "Body must be a object")
 
         service = AuthService()
         user = service.create(body)
@@ -54,6 +79,9 @@ def update_auth():
         if not body:
             raise HandlerException(400, "Not found body reequest")
 
+        if not isinstance(body, dict):
+            raise HandlerException(400, "Body must be a object")
+
         service = AuthService()
         user = service.update(g.user, body)
 
@@ -64,35 +92,79 @@ def update_auth():
         HandlerException(500, "Unexpected response: " + str(ex), str(ex)).abort()
 
 
-# @router.route("/expert", methods=["PUT"])
-# @login_required()
-# def update_auth_expert():
-#    """
-#    PUT: /api/v1/auth/expert
-#    """
-#    body = request.get_json()
-#
-#    if not body:
-#        return abort(400, description="NotFoundData", response="not-found-data")
-#
-#    _expert = g.user["b"].expert
-#
-#    if hasattr(body, "speciality"):
-#        ass_speciality = AssociationExpertToSpecialityService()
-#        ass_speciality.update_or_create_and_delete_many(_expert.id, body["speciality"])
-#
-#    if hasattr(body, "method"):
-#        ass_method = AssociationExpertToMethodService()
-#        ass_method.update_or_create_and_delete_many(_expert.id, body["method"])
-#
-#    if hasattr(body, "plan"):
-#        plan = PlanService()
-#        plan.update_or_create_and_delete_many(_expert.id, body["plan"])
-#
-#    service = ExpertService()
-#    expert = service.update(_expert.id, body)
-#
-#    return jsonify({"success": True, "response": expert})
+@router.route("/specialities", methods=["PUT"])
+@login_required()
+def update_auth_specialities():
+    """
+    PUT: /api/v1/auth/specialities
+    """
+    try:
+        body = request.get_json()
+
+        if not body:
+            raise HandlerException(400, "Not found body request")
+
+        if not isinstance(body, list):
+            raise HandlerException(400, "Body must be a array")
+
+        service = AuthService()
+        user = service.update(g.user, {"specialities": body})
+
+        return jsonify({"success": True, "response": user})
+    except HandlerException as ex:
+        ex.abort()
+    except Exception as ex:
+        HandlerException(500, "Unexpected response: " + str(ex), str(ex)).abort()
+
+
+@router.route("/methods", methods=["PUT"])
+@login_required()
+def update_auth_method():
+    """
+    PUT: /api/v1/auth/methods
+    """
+    try:
+        body = request.get_json()
+
+        if not body:
+            raise HandlerException(400, "Not found body request")
+
+        if not isinstance(body, list):
+            raise HandlerException(400, "Body must be a array")
+
+        service = AuthService()
+        user = service.update(g.user, {"methods": body})
+
+        return jsonify({"success": True, "response": user})
+    except HandlerException as ex:
+        ex.abort()
+    except Exception as ex:
+        HandlerException(500, "Unexpected response: " + str(ex), str(ex)).abort()
+
+
+@router.route("/plans", methods=["PUT"])
+@login_required()
+def update_auth_plan():
+    """
+    PUT: /api/v1/auth/plans
+    """
+    try:
+        body = request.get_json()
+
+        if not body:
+            raise HandlerException(400, "Not found body request")
+
+        if not isinstance(body, list):
+            raise HandlerException(400, "Body must be a array")
+
+        service = AuthService()
+        user = service.update(g.user, {"plans": body})
+
+        return jsonify({"success": True, "response": user})
+    except HandlerException as ex:
+        ex.abort()
+    except Exception as ex:
+        HandlerException(500, "Unexpected response: " + str(ex), str(ex)).abort()
 
 
 @router.route("/", methods=["PATCH"])
@@ -106,6 +178,9 @@ def update_field_auth():
 
         if not body:
             raise HandlerException(400, "Not found body request")
+
+        if not isinstance(body, dict):
+            raise HandlerException(400, "Body must be a object")
 
         service = AuthService()
         user = service.update_field(g.user, body)
