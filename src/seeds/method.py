158b651e3970f8541593ae111a63d5f0c81c4464
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-22 18:57:46 -0400

    fix(seeds): add handler exceptions

--- src/seeds/method.py
+++ src/seeds/method.py
@@ -2,6 +2,7 @@
 from faker import Faker
 
 from src.db import db
+from src.exceptions import HandlerException
 from src.models import Method
 
 
@@ -19,15 +20,25 @@ class MethodSeed:
 
     def up(self):
         """up"""
-        to_create = []
-        for item in payload:
-            method = Method(name=item)
-            to_create.append(method)
-        db.session.bulk_save_objects(to_create)
-        db.session.commit()
+        try:
+            to_create = []
+            for item in payload:
+                method = Method(name=item)
+                to_create.append(method)
+            db.session.bulk_save_objects(to_create)
+            db.session.commit()
+        except HandlerException as ex:
+            print(ex.message)
+        except Exception as ex:
+            print(ex, "error")
 
     def down(self):
         """down"""
-        db.session.query(Method).filter(Method.name.in_(payload)).delete(
-            synchronize_session=False
-        )
+        try:
+            db.session.query(Method).filter(Method.name.in_(payload)).delete(
+                synchronize_session=False
+            )
+        except HandlerException as ex:
+            print(ex.message)
+        except Exception as ex:
+            print(ex, "error")
