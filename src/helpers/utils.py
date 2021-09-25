db69ee48c75df13ae0ebd96f123e82b4f7e2a7db
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-25 08:01:40 -0400

    fix(helpers): add computed operator

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,7 +1,8 @@
 """ src.helpers.utils """
+import re
 from uuid import uuid4
 
-__all__ = ["inc", "dec", "generate_hash", "parse_order"]
+__all__ = ["inc", "dec", "generate_hash", "parse_order", "computed_operator"]
 
 
 def inc(i):
@@ -33,3 +34,37 @@ def parse_order(order):
         if __order == -1:
             return "desc"
     return order or None
+
+
+def computed_operator(column, v):
+    """
+    Computed operator, extracted from the query
+    value to assign it to a column
+
+    Args:
+        column (sqlalchemy.Column): column to assign operator
+        v (str): value with operator and value e.g `!=value`
+
+    Returns operator binary
+    """
+    if re.match(r"!=", v):
+        """__ne__"""
+        return column.__ne__(v)
+    if re.match(r">(?!=)", v):
+        """__gt__"""
+        return column.__gt__(v)
+    if re.match(r"<(?!=)", v):
+        """__lt__"""
+        return column.__lt__(v)
+    if re.match(r">=", v):
+        """__ge__"""
+        return column.__ge__(v)
+    if re.match(r"<=", v):
+        """__le__"""
+        return column.__le__(v)
+    if re.match(r"(\w*),(\w*)", v):
+        """between"""
+        a, b = re.split(r",", v)
+        return column.between(a, b)
+    """ default __eq__ """
+    return column.__eq__(v)
