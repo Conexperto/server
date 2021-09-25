7c2f4a5cadd4e1e027358c83e3b2397e1c00ceeb
Fernando Fernandez <frfernandezdev@gmail.com>
2021-09-25 08:01:40 -0400

    fix(helpers): add computed operator

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,8 +1,16 @@
 """ __init__ """
 from src.helpers.json_serializable import JSONSerializable
+from src.helpers.utils import computed_operator
 from src.helpers.utils import dec
 from src.helpers.utils import generate_hash
 from src.helpers.utils import inc
 from src.helpers.utils import parse_order
 
-__all__ = ["JSONSerializable", "inc", "dec", "generate_hash", "parse_order"]
+__all__ = [
+    "JSONSerializable",
+    "inc",
+    "dec",
+    "generate_hash",
+    "parse_order",
+    "computed_operator",
+]
