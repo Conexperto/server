aa5138bb749468e5bd4a86f4cd311f8aa64cd07a
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-06 12:27:22 -0400

    fix(ci): add private credentials firebase sdk

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Decrypt the file
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output ../../src/config/conexperto-6b5e7-sdk.json ../../src/config/conexperto-6b5e7-sdk.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output ./src/config/conexperto-admin-sdk.json ../../src/config/conexperto-admin-sdk.json.gpg
