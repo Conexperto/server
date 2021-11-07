fa008956cbe2bb07dd76f4f29ca0a80985559971
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-07 15:41:00 -0400

    ci: add input/output env to decrypt secret

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,7 +2,4 @@
 
 # Decrypt the file
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output ./src/config/conexperto-6b5e7-sdk.json ./src/config/conexperto-6b5e7-sdk.json.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output ./src/config/conexperto-admin-sdk.json ./src/config/conexperto-admin-sdk.json.gpg
+--output $OUTPUT $INPUT
