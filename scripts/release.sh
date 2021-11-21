d658821de2b5162b2e01580119bc41c5d759c9c1
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-21 13:55:01 -0400

    fix: change output release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,8 +2,8 @@
 
 # Decrypt for SDK website
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-	--output "./src/config/$FIREBASE_WEB_SDK_FILE.1" "./src/config/$FIREBASE_WEB_SDK_FILE.gpg"
+	--output "./src/config/$FIREBASE_WEB_SDK_FILE" "./src/config/$FIREBASE_WEB_SDK_FILE.gpg"
 
 # Decrypt for SDK admin
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
- --output "./src/config/$FIREBASE_ADMIN_SDK_FILE.1" "./src/config/$FIREBASE_ADMIN_SDK_FILE.gpg"
+ --output "./src/config/$FIREBASE_ADMIN_SDK_FILE" "./src/config/$FIREBASE_ADMIN_SDK_FILE.gpg"
