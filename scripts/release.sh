e46f6438126850d428da19412bd2cd3247d02270
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-21 14:30:18 -0400

    feat(cd): added decrypt sdk to jobs

--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Decrypt for SDK website
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-	--output "./src/config/$FIREBASE_WEB_SDK_FILE" "./src/config/$FIREBASE_WEB_SDK_FILE.gpg"
-
-# Decrypt for SDK admin
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
- --output "./src/config/$FIREBASE_ADMIN_SDK_FILE" "./src/config/$FIREBASE_ADMIN_SDK_FILE.gpg"
