ee5c5c55e1401d024da87d7fc8976dd20560d0f5
Fernando Fernandez <frfernandezdev@gmail.com>
2021-11-23 16:18:10 -0400

    refactor(ci): remove seeds

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -2,7 +2,5 @@
 set -euo pipefail
 
 FLASK_APP="src.api:create_api()" eval 'flask db upgrade'
-FLASK_APP="src.api:create_api()" eval 'flask seed user up'
-FLASK_APP="src.api:create_api()" eval 'flask seed admin up'
 
 exec flask run --host=0.0.0.0
