33ce3ff60105be1b285b17633c6689ef965826d0
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-07 12:17:48 -0400

    feat: added AppApiModule and AppBackOfficeModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppApiModule } from './apps/api/AppApiModule';
+import { AppBackOfficeModule } from './apps/backoffice/AppBackOfficeModule';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [__dirname + '/**/**/entities/*{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
-  controllers: [],
-  providers: [],
+  imports: [AppApiModule, AppBackOfficeModule],
 })
 export class AppModule {}
+
+//import { TypeOrmModule } from '@nestjs/typeorm';
+//TypeOrmModule.forRoot({
+//	type: 'sqlite',
+//	database: 'db',
+//	entities: [__dirname + '/**/**/entities/*{.ts,.js}'],
+//	synchronize: true,
+//}),
