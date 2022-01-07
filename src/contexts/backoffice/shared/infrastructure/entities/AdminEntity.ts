e9641c3e656faf3209feb04646c56e1877e3be3e
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-07 18:04:40 -0400

    feat(contexts): added AdminEntity

---
+++ src/contexts/backoffice/shared/infrastructure/entities/AdminEntity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import { RoleAdminEnumType } from 'src/contexts/backoffice/admins/domain/RoleAdminTypeEnum';
+
+@Entity({
+  name: 'cxp_admins',
+  orderBy: { id: 'DESC' },
+  synchronize: true,
+})
+export class AdminEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('uuid')
+  @Index({ unique: true })
+  uid: string;
+
+  @Column()
+  displayName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  photoURL: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  lastname: string;
+
+  @Column()
+  disabled: boolean;
+
+  @Column('enum', { enum: RoleAdminEnumType })
+  role: string;
+}
