52233594e515e735f4e6c2d6c40befea0a044b2f
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-07 21:40:07 -0400

    refactor(contexts): remove orderBy entity

--- src/contexts/backoffice/shared/infrastructure/entities/PlanEntity.ts
+++ src/contexts/backoffice/shared/infrastructure/entities/PlanEntity.ts
@@ -1,28 +1,26 @@
-import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
-
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({
-	name: 'cxp_plan',
-	orderBy: { id: 'DESC'},
-	synchronize: true
+  name: 'cxp_plan',
+  synchronize: true,
 })
 export class PlanEntity {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Column('uuid')
-	@Index({ unique: true})
-	uid: string;
+  @Column('uuid')
+  @Index({ unique: true })
+  uid: string;
 
-	@Column()
-	duration: number;
+  @Column()
+  duration: number;
 
-	@Column()
-	price: number;
+  @Column()
+  price: number;
 
-	@Column()
-	coin: string;
+  @Column()
+  coin: string;
 
-	@Column()
-	disabled: boolean;
+  @Column()
+  disabled: boolean;
 }
