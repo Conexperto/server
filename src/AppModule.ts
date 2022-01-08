6db8e7a5374b9cf80a4cf99231619602a3c2f464
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-08 10:42:42 -0400

    refactor(contexts): remove WinstonLogger

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppApiModule } from './apps/api/AppApiModule';
 import { AppBackOfficeModule } from './apps/backoffice/AppBackOfficeModule';
-import { WinstonLogger } from './shared/infrastructure/WinstonLogger';
+import { BackOfficeModule } from './contexts/backoffice/BackOfficeModule';
 
 @Module({
-  imports: [AppApiModule, AppBackOfficeModule, WinstonLogger],
+  imports: [AppApiModule, AppBackOfficeModule, BackOfficeModule],
 })
 export class AppModule {}
