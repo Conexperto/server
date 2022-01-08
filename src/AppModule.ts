d4001cb4d6c1c5ab3b5229c86a7a8a9a171d0d7e
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-08 00:07:43 -0400

    refactor: rename app module

---
+++ src/AppModule.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AppApiModule } from './apps/api/AppApiModule';
+import { AppBackOfficeModule } from './apps/backoffice/AppBackOfficeModule';
+import WinstonLogger from './contexts/shared/infrastructure/WinstonLogger';
+
+@Module({
+  imports: [AppApiModule, AppBackOfficeModule, WinstonLogger],
+})
+export class AppModule {}
