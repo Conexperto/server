30dab1a3d941ff6a0874024154806797788abb27
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-08 00:14:11 -0400

    refactor(contexts): remove SQLiteClientFactory exports to module

--- src/contexts/shared/SharedModule.ts
+++ src/contexts/shared/SharedModule.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { SQLiteClientFactory } from '../backoffice/shared/infrastructure/persistence/SQLiteClientFactory';
 import WinstonLogger from './infrastructure/WinstonLogger';
 
 @Module({
   imports: [],
   controllers: [],
   providers: [],
-  exports: [WinstonLogger, SQLiteClientFactory],
+  exports: [WinstonLogger],
 })
 export class SharedModule {}
