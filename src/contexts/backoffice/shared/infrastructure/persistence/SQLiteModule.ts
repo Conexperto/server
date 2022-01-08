453a8d1436100100ce7d93aa7b0bbd5b3eba8ee2
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-08 11:01:54 -0400

    feat(contexts): added SQLiteModule

---
+++ src/contexts/backoffice/shared/infrastructure/persistence/SQLiteModule.ts
@@ -0,0 +1,46 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminEntity } from '../entities/AdminEntity';
+import { AssociationUserToMethodEntity } from '../entities/AssociationUserToMethodEntity';
+import { AssociationUserToSpecialityEntity } from '../entities/AssociationUserToSpecialityEntity';
+import { MethodEntity } from '../entities/MethodEntity';
+import { PlanEntity } from '../entities/PlanEntity';
+import { SpecialityEntity } from '../entities/SpecialityEntity';
+import { UserEntity } from '../entities/UserEntity';
+import { UserExpertEntity } from '../entities/UserExpertEntity';
+import { UserRatingEntity } from '../entities/UserRatingEntity';
+
+const database = TypeOrmModule.forRoot({
+  type: 'sqlite',
+  database: 'cxp_db',
+  entities: [
+    AdminEntity,
+    UserEntity,
+    UserExpertEntity,
+    AssociationUserToSpecialityEntity,
+    AssociationUserToMethodEntity,
+    UserRatingEntity,
+    PlanEntity,
+    SpecialityEntity,
+    MethodEntity,
+  ],
+  synchronize: true,
+});
+
+const schemas = TypeOrmModule.forFeature([
+  AdminEntity,
+  UserEntity,
+  UserExpertEntity,
+  AssociationUserToSpecialityEntity,
+  AssociationUserToMethodEntity,
+  UserRatingEntity,
+  PlanEntity,
+  SpecialityEntity,
+  MethodEntity,
+]);
+
+@Module({
+  imports: [database, schemas],
+  exports: [database, schemas],
+})
+export class SQLiteModule {}
