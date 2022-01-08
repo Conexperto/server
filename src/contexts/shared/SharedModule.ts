8164ca2236c97e10d0a95426d7555001811a1127
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-08 00:11:21 -0400

    feat(contexts): added WinstonLogger and SQLiteClientFactory to exports module

--- src/contexts/shared/SharedModule.ts
+++ src/contexts/shared/SharedModule.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SQLiteClientFactory } from '../backoffice/shared/infrastructure/persistence/SQLiteClientFactory';
+import WinstonLogger from './infrastructure/WinstonLogger';
 
 @Module({
   imports: [],
   controllers: [],
   providers: [],
-  exports: [],
+  exports: [WinstonLogger, SQLiteClientFactory],
 })
 export class SharedModule {}
