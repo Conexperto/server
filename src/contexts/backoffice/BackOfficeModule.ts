0e3e749e3f9989e9fa9d05a6c20cc5f162852e5e
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-08 00:13:04 -0400

    feat(contexts): added SharedModule to imports module

--- src/contexts/backoffice/BackOfficeModule.ts
+++ src/contexts/backoffice/BackOfficeModule.ts
@@ -1,11 +1,11 @@
-import { Module } from "@nestjs/common";
-import { SQLiteClientFactory } from "./shared/infrastructure/persistence/SQLiteClientFactory";
-
+import { Module } from '@nestjs/common';
+import { SharedModule } from '../shared/SharedModule';
+import { SQLiteClientFactory } from './shared/infrastructure/persistence/SQLiteClientFactory';
 
 @Module({
-	imports: [SQLiteClientFactory],
-	controllers: [],
-	providers: [],
-	exports: []
+  imports: [SQLiteClientFactory, SharedModule],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class BackOfficeModule {}
