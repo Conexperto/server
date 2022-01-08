263f3a351a1310306b715fbdec419a4c4a227973
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-08 10:37:01 -0400

    refactor(contexts): remove SQLiteClientFactoryfrom shared backoffice contexts

--- src/contexts/backoffice/shared/infrastructure/persistence/SQLiteClientFactory.ts
+++
@@ -1,6 +0,0 @@
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import BackOfficeConfigFactory from '../config';
-
-export const SQLiteClientFactory = TypeOrmModule.forRoot(
-  BackOfficeConfigFactory.get('slite') as TypeOrmModuleOptions,
-);
