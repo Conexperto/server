e9e4bed24df47c4ee138769d320f516078632c7c
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-09 15:45:21 -0400

    feat(contexts): added new Operators

--- src/contexts/shared/domain/criteria/FilterOperator.ts
+++ src/contexts/shared/domain/criteria/FilterOperator.ts
@@ -6,8 +6,16 @@ export enum Operator {
   NOT_EQUAL = '!=',
   GT = '>',
   LT = '<',
+  GOET = '>=',
+  LOET = '<=',
   CONTAINS = 'CONTAINS',
   NOT_CONTAINS = 'NOT_CONTAINS',
+  LIKE = 'LIKE',
+  NOT_LIKE = 'NOT_LIKE',
+  ILIKE = 'ILIKE',
+  NOT_ILIKE = 'NOT_ILIKE',
+  BETWEEN = 'BETWEEN',
+  NOT_BETWEEN = 'NOT_BETWEEN',
 }
 
 export class FilterOperator extends EnumValueObject<Operator> {
@@ -25,10 +33,26 @@ export class FilterOperator extends EnumValueObject<Operator> {
         return new FilterOperator(Operator.GT);
       case Operator.LT:
         return new FilterOperator(Operator.LT);
+      case Operator.GOET:
+        return new FilterOperator(Operator.GOET);
+      case Operator.LOET:
+        return new FilterOperator(Operator.LOET);
       case Operator.CONTAINS:
         return new FilterOperator(Operator.CONTAINS);
       case Operator.NOT_CONTAINS:
         return new FilterOperator(Operator.NOT_CONTAINS);
+      case Operator.LIKE:
+        return new FilterOperator(Operator.LIKE);
+      case Operator.NOT_LIKE:
+        return new FilterOperator(Operator.NOT_LIKE);
+      case Operator.ILIKE:
+        return new FilterOperator(Operator.ILIKE);
+      case Operator.NOT_ILIKE:
+        return new FilterOperator(Operator.NOT_ILIKE);
+      case Operator.BETWEEN:
+        return new FilterOperator(Operator.BETWEEN);
+      case Operator.NOT_BETWEEN:
+        return new FilterOperator(Operator.NOT_BETWEEN);
       default:
         throw new InvalidArgumentError(
           `The filter operator ${value} is invalid`,
