1d1ac614d6ac73f4ca30c3c979c397a41c8dc427
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-09 18:54:31 -0400

    refactor(contexts): rename AdminName to BackofficeAdminName

--- src/contexts/backoffice/admins/domain/BackofficeAdminName.ts
+++ src/contexts/backoffice/admins/domain/BackofficeAdminName.ts
@@ -1,3 +1,3 @@
 import { StringValueObject } from 'src/contexts/shared/domain/value-object/StringValueObject';
 
-export class AdminName extends StringValueObject {}
+export class BackofficeAdminName extends StringValueObject {}
