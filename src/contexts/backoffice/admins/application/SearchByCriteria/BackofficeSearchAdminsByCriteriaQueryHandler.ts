32eb3e4cf1144b63172601f329d8be7c261ca500
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-10 00:08:18 -0400

    feat(contexts): added BackofficeSearchAdminsByCriteriaQueryHandler

---
+++ src/contexts/backoffice/admins/application/SearchByCriteria/BackofficeSearchAdminsByCriteriaQueryHandler.ts
@@ -0,0 +1,22 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { Filters } from 'src/contexts/shared/domain/criteria/Filters';
+import { Order } from 'src/contexts/shared/domain/criteria/Order';
+import { BackofficeAdminsResponse } from '../BackofficeAdminsResponse';
+import { BackofficeAdminsByCriteriaSearcher } from './BackofficeAdminsByCriteriaSearcher';
+import { BackofficeSearchAdminsByCriteriaQuery } from './BackofficeSearchAdminsByCriteriaQuery';
+
+@QueryHandler(BackofficeSearchAdminsByCriteriaQuery)
+export class BackofficeSearchAdminsByCriteriaQueryHandler
+  implements IQueryHandler<BackofficeSearchAdminsByCriteriaQuery>
+{
+  constructor(private readonly search: BackofficeAdminsByCriteriaSearcher) {}
+
+  async execute(
+    query: BackofficeSearchAdminsByCriteriaQuery,
+  ): Promise<BackofficeAdminsResponse> {
+    const filters = Filters.fromValues(query.filters);
+    const order = Order.fromValues(query.orderBy, query.orderType);
+
+    return this.search.run(filters, order, query.offset, query.limit);
+  }
+}
