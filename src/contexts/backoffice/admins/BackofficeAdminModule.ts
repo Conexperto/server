ddab518f71ccc09986bcd987fb6e2ff9ac6a3d9c
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-10 00:28:16 -0400

    feat(contexts): added providers to BackofficeAdminModule

--- src/contexts/backoffice/admins/BackofficeAdminModule.ts
+++ src/contexts/backoffice/admins/BackofficeAdminModule.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CreateBackofficeAdminCommandHandler } from './application/Create/CreateBackofficeAdminCommandHandler';
+import { BackofficeSearchAllAdminQueryHandler } from './application/SearchAll/BackofficeSearchAllAdminQueryHandler';
+import { BackofficeSearchAdminsByCriteriaQueryHandler } from './application/SearchByCriteria/BackofficeSearchAdminsByCriteriaQueryHandler';
+import { BackofficeSQLiteAdminRepository } from './infrastructure/persistence/BackofficeSQLiteAdminRepository';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [CqrsModule],
+  providers: [
+    BackofficeSQLiteAdminRepository,
+    CreateBackofficeAdminCommandHandler,
+    BackofficeSearchAllAdminQueryHandler,
+    BackofficeSearchAdminsByCriteriaQueryHandler,
+  ],
 })
 export class BackofficeAdminModule {}
