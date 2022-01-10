acb1fbc4cd73dda9776dfc55b073f896b337516a
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-09 23:50:30 -0400

    feat(contexts): added BackofficeAdminsByCriteriaSearcher

---
+++ src/contexts/backoffice/admins/application/SearchByCriteria/BackofficeAdminsByCriteriaSearcher.ts
@@ -0,0 +1,22 @@
+import { Criteria } from 'src/contexts/shared/domain/criteria/Criteria';
+import { Filters } from 'src/contexts/shared/domain/criteria/Filters';
+import { Order } from 'src/contexts/shared/domain/criteria/Order';
+import { BackofficeSQLiteAdminRepository } from '../../infrastructure/persistence/BackofficeSQLiteAdminRepository';
+import { BackofficeAdminsResponse } from '../BackofficeAdminsResponse';
+
+export class BackofficeAdminsByCriteriaSearcher {
+  constructor(private readonly repository: BackofficeSQLiteAdminRepository) {}
+
+  async run(
+    filters: Filters,
+    order: Order,
+    limit?: number,
+    offset?: number,
+  ): Promise<BackofficeAdminsResponse> {
+    const criteria = new Criteria(filters, order, limit, offset);
+
+    const admins = await this.repository.find(criteria);
+
+    return new BackofficeAdminsResponse(admins);
+  }
+}
