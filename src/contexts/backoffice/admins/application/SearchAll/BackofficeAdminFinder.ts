4722f2e498e212fadbe55c522f51f22304138452
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-10 00:10:32 -0400

    feat(contexts): added decorator injectable to BackofficeAdminFinder

--- src/contexts/backoffice/admins/application/SearchAll/BackofficeAdminFinder.ts
+++ src/contexts/backoffice/admins/application/SearchAll/BackofficeAdminFinder.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { BackofficeSQLiteAdminRepository } from '../../infrastructure/persistence/BackofficeSQLiteAdminRepository';
 import { BackofficeAdminsResponse } from '../BackofficeAdminsResponse';
 
+@Injectable()
 export class BackofficeAdminFinder {
   constructor(private readonly repository: BackofficeSQLiteAdminRepository) {}
 
