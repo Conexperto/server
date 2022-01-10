e56b12087d91b84098ab50cd1e0e152ca1efc093
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-10 00:38:11 -0400

    feat(contexts): added providers to BackofficeModule

--- src/contexts/backoffice/BackofficeModule.ts
+++ src/contexts/backoffice/BackofficeModule.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BackofficeAdminModule } from './admins/BackofficeAdminModule';
 import { BackofficeSQLiteModule } from './shared/infrastructure/persistence/BackofficeSQLiteModule';
 
 @Module({
   imports: [BackofficeSQLiteModule],
-  exports: [],
+  providers: [BackofficeAdminModule],
+  exports: [BackofficeAdminModule],
 })
 export class BackofficeModule {}
