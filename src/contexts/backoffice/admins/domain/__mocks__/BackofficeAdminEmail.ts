3069cef5f56761634d2e76e1fd594e84f28af6f1
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-11 15:42:12 -0400

    test(mocks): rename mock

--- src/contexts/backoffice/admins/domain/__mocks__/BackofficeAdminEmail.ts
+++ src/contexts/backoffice/admins/domain/__mocks__/BackofficeAdminEmail.ts
@@ -1,7 +1,7 @@
 import * as faker from 'faker';
 import { BackofficeAdminEmail } from '../BackofficeAdminEmail';
 
-export class BackofficeAdminEmailMock {
+class BackofficeAdminEmailMock {
   static create(value: string): BackofficeAdminEmail {
     return new BackofficeAdminEmail(value);
   }
@@ -10,3 +10,5 @@ export class BackofficeAdminEmailMock {
     return this.create(faker.internet.email());
   }
 }
+
+export { BackofficeAdminEmailMock as BackofficeAdminEmail }
