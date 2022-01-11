9caa84b00502bd1f970917286ba07c7e39c50ebb
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-11 10:52:06 -0400

    feat(contexts): added InjectRepository BackofficeSQLiteAdminRepository

--- src/contexts/backoffice/admins/application/Delete/BackofficeAdminDeleter.ts
+++ src/contexts/backoffice/admins/application/Delete/BackofficeAdminDeleter.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { BackofficeAdminId } from '../../domain/BackofficeAdminId';
 import { BackofficeSQLiteAdminRepository } from '../../infrastructure/persistence/BackofficeSQLiteAdminRepository';
 
 @Injectable()
 export class BackofficeAdminDeleter {
-  constructor(private readonly repository: BackofficeSQLiteAdminRepository) {}
+  constructor(
+    @InjectRepository(BackofficeSQLiteAdminRepository)
+    private readonly repository: BackofficeSQLiteAdminRepository,
+  ) {}
 
   async run(adminId: BackofficeAdminId[]): Promise<void> {
     const ids = adminId.map((obj) => obj.value);
