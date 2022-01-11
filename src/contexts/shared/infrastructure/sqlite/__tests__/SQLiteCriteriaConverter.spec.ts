7eb4fcc71b8c0fd777c30af9464aa5146d64739e
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-11 13:11:14 -0400

    test(contexts): addeed expecto for all itrs

--- src/contexts/shared/infrastructure/sqlite/__tests__/SQLiteCriteriaConverter.spec.ts
+++ src/contexts/shared/infrastructure/sqlite/__tests__/SQLiteCriteriaConverter.spec.ts
@@ -1,10 +1,7 @@
 import { Criteria } from 'src/contexts/shared/domain/criteria/Criteria';
 import { Filter } from 'src/contexts/shared/domain/criteria/Filter';
 import { FilterField } from 'src/contexts/shared/domain/criteria/FilterField';
-import {
-  FilterOperator,
-  Operator,
-} from 'src/contexts/shared/domain/criteria/FilterOperator';
+import { FilterOperator } from 'src/contexts/shared/domain/criteria/FilterOperator';
 import { Filters } from 'src/contexts/shared/domain/criteria/Filters';
 import { FilterValue } from 'src/contexts/shared/domain/criteria/FilterValue';
 import { Order } from 'src/contexts/shared/domain/criteria/Order';
@@ -13,10 +10,11 @@ import {
   OrderType,
   OrderTypes,
 } from 'src/contexts/shared/domain/criteria/OrderType';
+import { Equal } from 'typeorm';
 import { SQLiteCriteriaConverter } from '../SQLiteCriteraConverter';
 
 describe('SQLiteCriteriaConverter', () => {
-  it('should generate a query filter', () => {
+  it('should generate a query with offset and limit by default', () => {
     const filter = new Filter(
       new FilterField('email'),
       FilterOperator.fromValue('='),
@@ -28,8 +26,38 @@ describe('SQLiteCriteriaConverter', () => {
 
     const converter = new SQLiteCriteriaConverter();
 
-    const result = converter.convert(criteria);
+    const query = converter.convert(criteria);
 
-    console.log(result);
+    expect(query).toMatchObject({
+      take: 100,
+      order: { uid: 'asc' },
+      where: {
+        email: Equal('jhondeo@mail.com'),
+      },
+    });
+  });
+
+  it('should generate a query with offset and limit', () => {
+    const filter = new Filter(
+      new FilterField('email'),
+      FilterOperator.fromValue('='),
+      new FilterValue('jhondeo@mail.com'),
+    );
+    const order = new Order(new OrderBy('uid'), new OrderType(OrderTypes.ASC));
+    const filters = new Filters([filter]);
+    const criteria = new Criteria(filters, order, 200, 10);
+
+    const converter = new SQLiteCriteriaConverter();
+
+    const query = converter.convert(criteria);
+
+    expect(query).toMatchObject({
+      take: 200,
+      skip: 10,
+      order: { uid: 'asc' },
+      where: {
+        email: Equal('jhondeo@mail.com'),
+      },
+    });
   });
 });
