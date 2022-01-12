6baf3c0164c5b2f0ffc86945b4f7bf90a721db25
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-12 12:04:13 -0400

    feat(contexts): added method hasOrder to Criteria

--- src/contexts/shared/domain/criteria/Criteria.ts
+++ src/contexts/shared/domain/criteria/Criteria.ts
@@ -7,14 +7,23 @@ export class Criteria {
   readonly limit?: number;
   readonly offset?: number;
 
-  constructor(filters: Filters, order: Order, limit?: number, offset?: number) {
+  constructor(
+    filters: Filters,
+    order?: Order,
+    limit?: number,
+    offset?: number,
+  ) {
     this.filters = filters;
     this.order = order;
     this.offset = offset || 0;
-		this.limit = limit || 100;
+    this.limit = limit || 100;
   }
 
   public hasFilters(): boolean {
     return this.filters.filters.length > 0;
+  }
+
+  public hasOrder(): boolean {
+    return this.order ? this.order.hasOrder() : false;
   }
 }
