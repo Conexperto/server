d1dde2435736ddf68838d3429ba653594988b05e
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-12 17:22:49 -0400

    refactor(contexts): remove InjectRepository

--- src/contexts/backoffice/admins/application/Disabled/BackofficeAdminDisabler.ts
+++ src/contexts/backoffice/admins/application/Disabled/BackofficeAdminDisabler.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { BackofficeAdminId } from '../../domain/BackofficeAdminId';
 import { BackofficeSQLiteAdminRepository } from '../../infrastructure/persistence/BackofficeSQLiteAdminRepository';
 
 @Injectable()
 export class BackofficeAdminDisabler {
-  constructor(
-    @InjectRepository(BackofficeSQLiteAdminRepository)
-    private readonly repository: BackofficeSQLiteAdminRepository,
-  ) {}
+  constructor(private readonly repository: BackofficeSQLiteAdminRepository) {}
 
   async run(adminId: BackofficeAdminId[]): Promise<void> {
     const ids = adminId.map((obj) => obj.value);
