b18ee89432f9bdd6033b3900a9f688be04c98e6a
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-13 12:08:37 -0400

    refactor(contexts): remove InjectRepository

--- src/contexts/backoffice/admins/application/SearchAll/BackofficeAdminFinder.ts
+++ src/contexts/backoffice/admins/application/SearchAll/BackofficeAdminFinder.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { BackofficeSQLiteAdminRepository } from '../../infrastructure/persistence/BackofficeSQLiteAdminRepository';
 import { BackofficeAdminsResponse } from '../BackofficeAdminsResponse';
 
 @Injectable()
 export class BackofficeAdminFinder {
-  constructor(
-    @InjectRepository(BackofficeSQLiteAdminRepository)
-    private readonly repository: BackofficeSQLiteAdminRepository,
-  ) {}
+  constructor(private readonly repository: BackofficeSQLiteAdminRepository) {}
 
-  async run() {
+  async run(): Promise<BackofficeAdminsResponse> {
     const admins = await this.repository.findAll();
 
     return new BackofficeAdminsResponse(admins);
