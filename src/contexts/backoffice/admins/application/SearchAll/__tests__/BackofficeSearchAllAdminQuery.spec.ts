b90841ce4261e7eaae18cd22ff9f784ccf04f285
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-13 12:05:51 -0400

    test(contexts): refactor tests

---
+++ src/contexts/backoffice/admins/application/SearchAll/__tests__/BackofficeSearchAllAdminQuery.spec.ts
@@ -0,0 +1,10 @@
+import { BackofficeSearchAllAdminQuery } from "../BackofficeSearchAllAdminQuery"
+
+
+describe('BackofficeSearchAllAdminQuery', () => {
+	it('should search all query', () => {
+		const query = new BackofficeSearchAllAdminQuery();
+		
+		expect(query).toBeInstanceOf(BackofficeSearchAllAdminQuery);
+	})
+})
