08b3a0a431cfc05a1bb9c87a5e6ad28dab65c1d9
Fernando Fernandez <frfernandezdev@gmail.com>
2022-01-19 08:04:15 -0400

    test(contexts): added BackofficeSearchPlansByCriteriaQuery test

---
+++ src/contexts/backoffice/plans/application/SearchByCriteria/__tests__/BackofficeSearchPlansByCriteriaQuery.spec.ts
@@ -0,0 +1,29 @@
+import { BackofficeSearchPlansByCriteriaQuery } from '../BackofficeSearchPlansByCriteriaQuery';
+
+describe('BackofficeSearchPlansByCriteriaQuery', () => {
+  it('should search by criteria query', () => {
+    const filter = new Map();
+    filter.set('field', 'id');
+    filter.set('operator', '=');
+    filter.set('value', 'id');
+
+    const orderBy = 'id';
+    const orderType = 'ASC';
+    const limit = 10;
+    const offset = 0;
+    const query = new BackofficeSearchPlansByCriteriaQuery(
+      [filter],
+      orderBy,
+      orderType,
+      limit,
+      offset,
+    );
+
+    expect(query).toBeInstanceOf(BackofficeSearchPlansByCriteriaQuery);
+    expect(query.filters).toEqual([filter]);
+    expect(query.orderBy).toBe(orderBy);
+    expect(query.orderType).toBe(orderType);
+    expect(query.limit).toBe(limit);
+    expect(query.offset).toBe(offset);
+  });
+});
